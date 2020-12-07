2564ecd496b643f686aa0b5f04fc76dc46fbae76
hipek <puaro@vp.pl>
2020-12-07 14:07:39 +0100

    zmiany

--- src/About.jsx
+++ src/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <Commons
         name="Razem z rodzicami Marianną i Józefem"
         description="W drewnianym domu przy ul. Solnej mieszkaliśmy 5 lat. Tutaj zacząłem rysować konie w bardzo oszczędnej, lapidarnej formie - (zbliżone nieco do naskalnych piktogramów z Alty - jeśli ktoś nie wie, jak wyglądały);
-        uczyłem się, ale bez powodzenia, grać na akordeonie. Lepiej wypadła mi jazda na rowerze. Największe wrażenie wywarły na mnie elementarz Falskiego i budynek z czerwonej cegły szkoły podstawowej nr 27, wyjęty jakby żywcem z tego kultowego dziś podręcznika. Ostatecznie naukę podjąłem w innej, jeszcze bardziej szacownej szkole nr 11 przy ul. Szkolnej (obecnie J. Grzecznarowskiego)."
+        uczyłem się, ale bez powodzenia, grać na akordeonie. Lepiej wypadła mi jazda na rowerze. Największe wrażenie wywarły na mnie elementarz Falskiego i pobliski budynek z czerwonej cegły szkoły podstawowej nr 27, wyjęty jakby żywcem z tego kultowego dziś podręcznika. Ostatecznie naukę podjąłem w innej, jeszcze bardziej szacownej szkole nr 11 przy ul. Szkolnej (obecnie J. Grzecznarowskiego)."
         imgsrc={web1}
         visit="/contact"
         btname="Więcej"
