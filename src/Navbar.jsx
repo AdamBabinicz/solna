1452e31c071a28e093742d9b6cf4a1eb57b80fe7
hipek <puaro@vp.pl>
2020-12-08 14:33:01 +0100

    zmiana ikonki

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,6 +6,7 @@ import EmailIcon from "@material-ui/icons/Email";
 import InfoIcon from "@material-ui/icons/Info";
 import ImageIcon from "@material-ui/icons/Image";
 import ChildFriendlyIcon from "@material-ui/icons/ChildFriendly";
+import CameraIcon from "@material-ui/icons/Camera";
 
 const Navbar = () => {
   return (
@@ -67,7 +68,7 @@ const Navbar = () => {
                     className="nav-link"
                     to="/slider"
                   >
-                    <EmailIcon /> Album
+                    <CameraIcon /> Album
                   </NavLink>
                 </li>
               </ul>
