878bb3162aa6db132bd843b7f3b98b987f4fb756
hipek <puaro@vp.pl>
2020-12-08 11:14:40 +0100

    zmiany w tekście

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Commons from "./Commons";
 import web from "./images/1.png";
+import ScrollToTop from "./ScrollToTop";
 
 const Home = () => {
   return (
-    <Commons
-      name="Jedno z moich pierwszych zdjęć"
-      description="Przyszedłem na świat dokładnie 100 lat później, niż Nicola Tesla, jako drugie dziecko po śmierci mojego brata
-      Witolda, który przeżył zaledwie jeden rok.
-      Imię Adam miało uchronić mnie przed przedwczesną śmiercią. Jako małe dziecko byłem bardzo chorowity - jak twierdzili rodzice, z powodu wilgoci i grzyba panujących w wynajętym pokoju."
-      imgsrc={web}
-      visit="/gallery"
-      btname="Więcej"
-    ></Commons>
+    <>
+      <ScrollToTop />
+      <Commons
+        name="Jedno z moich pierwszych zdjęć"
+        description="Przyszedłem na świat dokładnie 100 lat później, niż Nicola Tesla, jako drugie dziecko po śmierci mojego brata
+      Witolda, który przeżył zaledwie jeden rok."
+        description1="Imię Adam miało uchronić mnie przed przedwczesną śmiercią. Jako małe dziecko byłem bardzo chorowity - jak twierdzili rodzice, z powodu wilgoci i grzyba panujących w wynajętym pokoju."
+        imgsrc={web}
+        visit="/gallery"
+        btname="Więcej"
+      ></Commons>
+    </>
   );
 };
 
