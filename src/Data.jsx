2a24572124932b08f669f27ebf42d9a41bf676ee
babinicz <babinicz@onet.pl>
2021-06-30 18:55:45 +0200

    dodałem fotki

--- src/Data.jsx
+++ src/Data.jsx
@@ -5,6 +5,11 @@ import web3 from "./images/6.jpg";
 import web4 from "./images/7.jpg";
 import web5 from "./images/9.jpg";
 import img1 from "./images/27.jpg";
+import img2 from "./images/29.jpg";
+import img3 from "./images/30.jpg";
+import img4 from "./images/31.jpg";
+import img5 from "./images/32.jpg";
+import img6 from "./images/33.jpg";
 
 const Data = [
   {
@@ -22,30 +27,50 @@ const Data = [
     title: "Mój starszy kolega",
     content:
       "Zbyszek był o kilka lat starszy ode mnie. Jego rodzice przyjaźnili się z moimi.",
+    img: img2,
+    Title: "Mój pierwszy drink",
+    p: "Podczas przyjęcia z okazji chrzcin mojej siostry, gdy goście poszli tańczyć, podszedłem do stołu i dopiłem wszystkie kieliszki, w których cokolwiek się znajdowało. Moja przygoda z mocnymi napojami zakończyła się 2-dniowym snem. Miałem wtedy ponad 2 lata.",
+    em: "",
   },
   {
     imgsrc: web2,
     title: "Na łące z rodzicami",
     content:
       "Łąka ta służyła nam nie tylko do przechadzek, ale również zaopatrywała nas w świeży szczaw, chrzan i koper do kiszenia ogórków.",
+    img: img3,
+    Title: "Nauka jazdy na rowerze",
+    p: "Jako jeden z pierwszych na ulicy miałem rower. Pierwsza udana jazda wyglądała w ten sposób, że zachowując równowagę, przejechałem w poprzek ulicy Solnej i wjechałem na podwórko Ciszków, ale tylko dlatego, że furtka była otwarta. Niestety nie umiałem jeszcze hamować.",
+    em: "",
   },
   {
     imgsrc: web3,
     title: "Z ojcem",
     content:
       "W najbliższej okolicy nie było żdnego ruchu zmotoryzowanego. Na spacer wychodziłem z ojcem lub sam.",
+    img: img4,
+    Title: "Przygoda z płotem",
+    p: "Chodziłem do sąsiadów na telewizję oglądać bajkę na dobranoc „Gąska Balbinka”. Oczywiście obrazki były nieruchome. Pewnego razu gospodyni P. Potemska zamknęła furtkę i musiałem przejść przez płot. Gdy byłem już po drugiej stronie, zaczepiłem podbródkiem o szpic sztachety i nie mogłem się uwolnić. Już nie pamiętam, kto mnie ściągnął z płotu.",
+    em: "",
   },
   {
     imgsrc: web4,
     title: "Z łyżką",
     content:
       "Łyżką do zupy kopałem w piachu, który wówczas był nawierzchnią wielu bocznych, niebrukowanych ulic Radomia.",
+    img: img5,
+    Title: "Koń na biegunach",
+    p: "Samego konia nie pamiętam, natomiast jego bujną grzywę - jak najbardziej. Z powodu tej grzywy, na którą reagowałem panicznym lękiem, rodzice pozbyli się bujanki. W zamian dostałem drewnianą bryczkę z dwoma konikami kupioną na jarmarku. Gdy ciągnąłem na sznurku koniki, jeden z woźniców obecnych na Korei, zamachnął się batem i uciął łeb jednemu z koni. Byłem tak zrozpaczony, że rodzice wrócili do sprzedawcy i kupili mi nową bryczkę.",
+    em: "",
   },
   {
     imgsrc: web5,
     title: "Powrót do domu",
     content:
       "Po wypróbowaniu jak smakuje piach z ul. Bakermanów (obecnie Parkowej), wracałem na swoje podwórko na ul. Solnej.",
+    img: img6,
+    Title: "Motocykl za migdałka",
+    p: "Z powodu moich problemów z migdałkiem środkowym, udałem się z rodzicami do gabinetu zabiegowego w kamienicy na Placu Konstytucji na drugim lub trzecim piętrze. Siedziałem już na fotelu, ale skorzystałem z nieuwagi lekarza i asystentki, i wybiegłem z gabinetu. Po schodach zbiegłem na dół i dopiero na ulicy dogonił mnie mój ojciec. W nagrodę za pozbycie się migdałka dostałem mały plastikowy motocykl.",
+    em: "",
   },
 ];
 
